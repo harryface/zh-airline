248ca506e7c9cc1659a5a6b3397599addf646c5e
Harrison <harryilonze@yahoo.com>
2021-09-09 11:04:10 +0100

    Aeroplane model

--- airline/main/models.py
+++ airline/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Aeroplane(models.Model):
+    aeroplane_id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    id = models.IntegerField(primary_key=False)
+
+    def __str__(self):
+        return self.name
--- airline/main/tests.py
+++ airline/main/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+
+from .models import Aeroplane
 
 # Create your tests here.
+
+class AeroplaneModelTest(TestCase):
+    '''Test Aeroplane Models'''
+    
+    def setUp(self):
+        Aeroplane.objects.create(
+                name='Boeing 737',
+                id=5
+            )
+        Aeroplane.objects.create(
+                name='Airbus A350',
+                id=4
+            )
+
+    def test_aeroplane_instance(self):
+        airbus = Aeroplane.objects.get(id='4')
+        boeing = Aeroplane.objects.get(name='Boeing 737')
+        self.assertEqual(airbus.id, 4)
+        self.assertEqual(boeing.id, 5)
+        self.assertEqual(boeing.__str__(), boeing.name)
